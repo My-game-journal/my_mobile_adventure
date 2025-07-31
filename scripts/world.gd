5b97c28a16ae4ed9f01bf401d211a9792629422d
My-game-journal <milosztelega1@gmail.com>
2025-07-31 19:55:06 +0200

    Changes after break.

--- scripts/world.gd
+++ scripts/world.gd
@@ -1,9 +1,5 @@
 extends Node
 
-func _ready():
-	$ThemePlayer.process_mode = Node.PROCESS_MODE_ALWAYS
-	$ThemePlayer.play()
-
 func _input(event):
 	if event.is_action_pressed("pause_menu_button"):
 		$CanvasLayer/PausedMenu.visible = true
