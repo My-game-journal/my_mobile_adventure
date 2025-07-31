14b3c3bc057b7647dc1fb9271eeff8a84a9f7c8e
My-game-journal <milosztelega1@gmail.com>
2025-07-31 20:28:21 +0200

    Controls.

--- scripts/mobile_controls.gd
+++ scripts/mobile_controls.gd
@@ -1,18 +1,9 @@
 extends CanvasLayer
 
-@onready var pause_button = $ControlPause/ControlPauseButton
-@onready var move_left_button = $ControlMove/ControlMoveLeftButton
-@onready var move_right_button = $ControlMove/ControlMoveRightButton
-@onready var shield_button = $ControlAction/ControlShieldButton
-@onready var roll_button = $ControlAction/ControlRollButton
-@onready var jump_button = $ControlAction/ControlJumpButton
-@onready var attack_button = $ControlAction/ControlAttackButton
-
-var is_mobile_platform: bool = false
 var mobile_controls_enabled: bool = true
 
 func _ready():
-	is_mobile_platform = OS.get_name() in ["Android", "iOS"]
+	var is_mobile_platform = OS.get_name() in ["Android", "iOS"]
 	set_mobile_controls_visibility(is_mobile_platform or mobile_controls_enabled)
 	layer = 100
 
